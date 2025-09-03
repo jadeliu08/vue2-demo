6d1e0c11de2f73170a674d732bdcc1e2ae385ce6
jadeliu08 <changqingliu08@163.com>
2025-09-03 19:24:45 +0800

    update

---
+++ src/components/HelloWorld/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msgDesc }}</h1>
+    <h2>you are so stupid</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: false,
+      default: '123',
+    }
+  },
+  computed: {
+    msgDesc() {
+      console.log('this', this);
+      return this.msg + 'ssssss';
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./styles.css" />
